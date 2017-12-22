3110c8075699b75cedd04542726203947ca22623
Shinmaul <maulana.husni@gmedia.co.id>
2017-12-22 14:49:00 +0700

    update berkala

---
+++ app/src/main/java/gmedia/net/id/pullens/LoginActivity.java
@@ -0,0 +1,19 @@
+package gmedia.net.id.pullens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+    }
+}
